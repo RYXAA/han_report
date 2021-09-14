f14c3afe49d0857692277982999dbff2e310ff03
RYXAA <77099342+RYXAA@users.noreply.github.com>
2021-09-14 23:32:05 +0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+Config.ReportCommand = "report" -- Perintah yang digunakan pemain untuk melaporkan sesuatu (hapus baris atau beri tanda "" untuk menonaktifkan perintah)
+
+Config.ReportListCommand = "reportlist" -- Perintah yang digunakan STAFF untuk memeriksa laporan (hapus baris atau beri tanda "" untuk menonaktifkan perintah)
+
+Config.ESX = true --Server ESX? Itu hanya digunakan untuk mendefinisikan staff dengan lebih mudah
+
+Config.TakeScreenshot = "https://discord.com/api/webhooks/869986128697098250/gSMn3qBcvuL14OFW3-kSGocfHd5eRAPTlglE4ax3Sdam47FeMY1AbvtguPsofH6B-R28" -- ubah ini ke tautan webhook perselisihan, atau hapus jika Anda tidak ingin mengambil tangkapan layar
+
+Config.Admins = { --Jika Anda tidak menggunakan ESX, Anda harus meletakkan di sini steamid/lisensi/apa pun dari admin Anda (periksa beberapa contoh)
+	"steam:11111",
+	"license:1111",
+	"xbl:111",
+	"live:111",
+	"discord:11",
+	"fivem:111",
+	"ip:0.0.0.0", -- taruh ip lu disini
+}
+
+
+
+
+
